16e8c6d04af3074cac96a639a04df29e74d639a0
Klipfou <nos_67210@hotmail.fr>
2020-04-17 15:58:58 +0200

    gestion du double click et dispariton text

--- panel.js
+++ panel.js
@@ -2,13 +2,15 @@ let panels = document.querySelectorAll('.panel');
 
 function toggleOpen() {
     this.classList.toggle('open');
-
 };
 
 function toggleActive(e) {
     if (e.propertyName.includes('flex')) {
         this.classList.toggle('active');
     }
+    if (!this.classList.contains('open') && this.classList.contains('open-active')) {
+        this.classList.remove('open-active');
+    };
 }
 
 
