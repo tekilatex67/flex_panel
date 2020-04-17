bb779eb7a1506c3754b18e7e55bd2a915833c412
Klipfou <nos_67210@hotmail.fr>
2020-04-17 15:06:57 +0200

    flex pannel complet

---
+++ panel.js
@@ -0,0 +1,15 @@
+let panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+    this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+    if (e.propertyName.includes('flex')) { //flex a cause du flex grow
+        this.classList.toggle('open-active')
+    }
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
